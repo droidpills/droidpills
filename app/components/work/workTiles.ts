3daa4e30822347c0ed77ed09be946ff045873d55
Marco Lima <marco.lima@qr.capital>
2025-05-14 15:10:47 -0300

    feat: portfolio

--- app/components/work/workTiles.ts
+++ app/components/work/workTiles.ts
@@ -13,29 +13,29 @@ export const workTiles: WorkTile[] = [
   {
     description: `Here are some projects`,
     title: `we've built.`,
-    link: 'https://mesascout.com.br/',
+    link: 'https://cca.blocktrends.com.br/',
     image: {
-      src: '/static/images/mesa-scout.png',
+      src: '/static/images/project/cca.png',
       width: 900,
       height: 900,
     },
   },
-   {
-     description: `See our work`,
-     title: `in action.`,
-     link: 'http://etalent.com.br/',
-     image: {
-       src: '/static/images/project/etalent.jpg',
-       width: 900,
-       height: 900,
-     },
-   },
-   {
+  {
+    description: `See our work`,
+    title: `in action.`,
+    link: 'https://www.blocktrends.app/',
+    image: {
+      src: '/static/images/project/btapp.png',
+      width: 900,
+      height: 900,
+    },
+  },
+  {
     description: `Check out our`,
     title: `latest projects.`,
-    link: 'https://impecavel.com.br/',
+    link: 'https://mesascout.com.br/',
     image: {
-      src: '/static/images/project/impecavel.jpg',
+      src: '/static/images/project/mesa-scout-home.png',
       width: 900,
       height: 900,
     },
--- app/projects/constants.ts
+++ app/projects/constants.ts
@@ -2,20 +2,34 @@ import { Project } from './types';
 
 export const projects: Project[] = [
   {
+    title: 'Certificação Criptoativos',
+    src: 'cca.png',
+    color: '#dbeafe',
+    url: 'https://cca.blocktrends.com.br/',
+    role: 'Full Stack Development',
+  },
+  {
+    title: 'Blocktrends APP',
+    src: 'btapp.png',
+    color: '#dbeafe',
+    url: 'https://www.blocktrends.app/',
+    role: 'Full Stack Development',
+  },
+  {
     title: 'Mesa Scout',
     src: 'mesa-scout-home.png',
     color: '#dbeafe',
     url: 'https://mesascout.com.br/',
     role: 'Data Extraction & Automation',
   },
-   {
-     title: 'Etalent',
-     src: 'etalent.jpg',
-     color: '#dbeafe',
-     url: 'http://etalent.com.br/',
-     role: 'Full Stack Development',
-   },
-   {
+  {
+    title: 'Etalent',
+    src: 'etalent.jpg',
+    color: '#dbeafe',
+    url: 'http://etalent.com.br/',
+    role: 'Full Stack Development',
+  },
+  {
     title: 'Impecavel',
     src: 'impecavel.jpg',
     color: '#dbeafe',
